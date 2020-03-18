8de2e27b82abdf5482cf365a5f6b43c79dad7142
Marijana Markov <marijana.z.markov@gmail.com>
2020-03-18 14:24:18 +0100

    add new test class

---
+++ UserRegistrationMVC5/UserRegistrationMVC5.Tests/Controllers/UsersControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UserRegistrationMVC5;
+using UserRegistrationMVC5.Controllers;
+
+namespace UserRegistrationMVC5.Tests.Controllers
+{
+    [TestClass]
+    class UsersControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            UsersController controller = new UsersController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
